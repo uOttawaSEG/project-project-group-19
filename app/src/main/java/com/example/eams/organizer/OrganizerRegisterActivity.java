b4fbfc1140a34e8f270454d563913e9d67a60257
naomi <naomibraun321@gmail.com>
2024-10-09 16:22:44 -0400

    Updates to layouts

--- app/src/main/java/com/example/eams/organizer/OrganizerRegisterActivity.java
+++ app/src/main/java/com/example/eams/organizer/OrganizerRegisterActivity.java
@@ -16,7 +16,7 @@ public class OrganizerRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_organizer_register);
+        setContentView(R.layout.activity_organizer_welcome_admin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
