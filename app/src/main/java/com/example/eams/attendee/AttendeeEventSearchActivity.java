37bf464b079e25538d06a83bf86d5a2a05b032b4
ajersch <alexandrus3765@gmail.com>
2024-12-03 22:52:10 -0500

    Made search results appear by default

--- app/src/main/java/com/example/eams/attendee/AttendeeEventSearchActivity.java
+++ app/src/main/java/com/example/eams/attendee/AttendeeEventSearchActivity.java
@@ -81,5 +81,7 @@ public class AttendeeEventSearchActivity extends AppCompatActivity {
                 adapter.setEvents(newEvents);
             });
         });
+
+        searchButton.performClick();
     }
 }
